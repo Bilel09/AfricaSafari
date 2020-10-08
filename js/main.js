7f3e297ba45ccf4b8012b8a1caa39e79453fb505
Bilel Mezhoud <34798758+Bilel09@users.noreply.github.com>
2020-10-08 22:31:34 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+  $(".toggle-accordion").on("click", function() {
+    var accordionId = $(this).attr("accordion-id"),
+      numPanelOpen = $(accordionId + ' .collapse.in').length;
+    
+    $(this).toggleClass("active");
+
+    if (numPanelOpen == 0) {
+      openAllPanels(accordionId);
+    } else {
+      closeAllPanels(accordionId);
+    }
+  })
+
+  openAllPanels = function(aId) {
+    console.log("setAllPanelOpen");
+    $(aId + ' .panel-collapse:not(".in")').collapse('show');
+  }
+  closeAllPanels = function(aId) {
+    console.log("setAllPanelclose");
+    $(aId + ' .panel-collapse.in').collapse('hide');
+  }
+     
+});
+//////////////////////////////////////////////////////////////////////////
+function printDiv(divID)  
+{ 
+        var divElements = document.getElementById(divID).innerHTML; 
+        var oldPage = document.body.innerHTML; 
+
+        document.body.innerHTML =  
+          "<html><head><title></title></head><body>" +  
+          divElements + "</body>"; 
+
+        window.print(); 
+
+        document.body.innerHTML = oldPage; 
+    } 
